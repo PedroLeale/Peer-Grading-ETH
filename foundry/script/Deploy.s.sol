d4476f7662026587eeeb6062a62b9739a723e4e5
pedro bufulin <pedro.almeida@ufu.br>
2023-06-22 18:31:19 -0300

    makefile, deploy script, toml

---
+++ foundry/script/Deploy.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+/* solhint-disable no-global-import */
+import "forge-std/Script.sol";
+import "../src/PeerGradingContract.sol";
+
+/* solhint-enable no-global-import */
+
+contract Deploy is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.stopBroadcast();
+        console.log("contract address: %s", address(moderatio));
+    }
+}
