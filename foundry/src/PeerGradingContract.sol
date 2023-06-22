31e173bf10cd0d93409d5cc17c1f249c8a91bb75
pedro bufulin <pedro.almeida@ufu.br>
2023-06-22 18:06:13 -0300

    feat: generate seed

--- foundry/src/PeerGradingContract.sol
+++ foundry/src/PeerGradingContract.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.13;
 contract PeerGradingContract {
     mapping(address => Participant) public participants;
 
+    uint256 globalSeed;
+    uint256 num_participants;
+    uint256[] entropies;
+
+    error AlreadyScrambled();
+
     struct Participant {
         uint256 entropy;
         uint256[] assigned;
@@ -18,7 +24,18 @@ contract PeerGradingContract {
             Participant storage participant = participants[_participants[i]];
             participant.assigned = _assignments[i];
         }
+        num_participants = _participants.length;
     }
 
-    function generateEntropy(uint256 _entropy) public {}
+    function generateEntropy(uint256 _entropy) public {
+        Participant storage participant = participants[msg.sender];
+
+        if (participant.entropy != 0) revert AlreadyScrambled();
+        participant.entropy = _entropy;
+        entropies.push(_entropy);
+    }
+
+    function generateSeed() public {
+        globalSeed = uint256(keccak256(abi.encodePacked(entropies)));
+    }
 }
--- foundry/test/Contract.t.sol
+++ foundry/test/Contract.t.sol
@@ -12,17 +12,27 @@ contract TestPeerGrading is Test {
     uint8[][] assignemnts;
 
     function setUp() public {
-        participants = new address[](5);
+        participants = [address(1), address(2), address(3), address(4), address(5)];
+
         assignemnts = [[2, 3, 4], [1, 3, 5], [1, 2, 4], [2, 3, 5], [1, 3, 4]];
         c = new PeerGradingContract(participants, assignemnts);
     }
 
-    function testGenerateEntropy() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
+    //it should generate am entropy value for each particiapnt and
+    //then generate the global seed
+    function test_GenerateEntropy(uint256 _entropy) public {
+        uint256[] memory numbers = new uint256[](participants.length);
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+        for (uint256 i = 0; i < participants.length; i++) {
+            _entropy = uint256(keccak256(abi.encode(_entropy))); // Generate a new _entropy using the previous _entropy
+            numbers[i] = _entropy % 100; // Generate a number between 0 and 99 using modular arithmetic
+        }
+
+        for (uint256 i = 0; i < participants.length; i++) {
+            vm.prank(participants[i]);
+            c.generateEntropy(numbers[i]);
+        }
+
+        c.generateSeed();
     }
 }
