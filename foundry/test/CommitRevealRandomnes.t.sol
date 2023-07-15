d61d84ba68e58088e7d0fc634710e18c251ddccd
pedro bufulin <pedro.almeida@ufu.br>
2023-07-15 19:08:59 -0300

    gen snapshot command, test only randomness file

---
+++ foundry/test/CommitRevealRandomnes.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "src/CommitRevealRandomness.sol";
+import {CommitUtils} from "src/CommitUtils.sol";
+
+contract CommitReveralRandomnessTest is Test {
+    CommitReveralRandomness randSrc;
+
+    address[] participants;
+    uint8[] assignemnts;
+
+    error NotAParticipant();
+
+    function setUp() public {
+        participants = [address(1), address(2), address(3), address(4), address(5)];
+        randSrc = new CommitReveralRandomness(participants);
+    }
+
+    // tests the complete logic as we intend it to work
+    function test_full_logic(uint256 _entropy) public {
+        uint256[] memory numbers = new uint256[](participants.length);
+
+        for (uint256 i = 0; i < participants.length; i++) {
+            _entropy = uint256(keccak256(abi.encode(_entropy))); // Generate a new _entropy using the previous _entropy
+            numbers[i] = _entropy % 100; // Generate a number between 0 and 99 using modular arithmetic
+        }
+
+        bytes32 com = CommitUtils.createCommitment(participants[0], numbers[0]);
+        vm.expectRevert(abi.encodeWithSelector(NotAParticipant.selector));
+        randSrc.commit(com);
+
+        for (uint256 i = 0; i < participants.length; i++) {
+            vm.prank(participants[i]);
+            com = CommitUtils.createCommitment(participants[i], numbers[i]);
+            vm.prank(participants[i]);
+            randSrc.commit(com);
+        }
+
+        for (uint256 i = 0; i < participants.length; i++) {
+            vm.prank(participants[i]);
+            randSrc.reveal(numbers[i]);
+        }
+    }
+}
