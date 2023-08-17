ee3256b04a514bead676d575e5a04c76f898d28a
pedro bufulin <pedro.almeida@ufu.br>
2023-08-17 14:43:40 -0300

    fix eslint

--- front-end/components/TextBorder/index.tsx
+++ front-end/components/TextBorder/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Image } from '@chakra-ui/react';
+import React from "react";
+import { Box, Image } from "@chakra-ui/react";
 
 export const TextBorder = ({
   children,
--- front-end/pages/home.tsx
+++ front-end/pages/home.tsx
@@ -1,22 +1,26 @@
 import { Navbar } from "@/components/Navbar";
 import { TextBorder } from "../components/TextBorder";
-import { Flex, Image, Box } from '@chakra-ui/react'
+import { Flex, Image, Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <><Navbar />
-    <div className=" w-1000 h-screen flex justify-center items-center">
+    <>
+      <Navbar />
+      <div className=" w-1000 h-screen flex justify-center items-center">
         <Flex direction="row">
-        <TextBorder children={"Peer Grading text 1"} imageSrc={"/assets/security-icon.png"}/>
-        <Box w='70px' h='10'/>
-        <TextBorder children={"Peer Grading text 2"} imageSrc={"/assets/verified-icon.png"}/>
-        <Box w='70px' h='10'/>
-        <Image
-            src="/assets/first-page-image.png"
-            w="500px"
-            h="600px"
+          <TextBorder
+            children={"Peer Grading text 1"}
+            imageSrc={"/assets/security-icon.png"}
           />
+          <Box w="70px" h="10" />
+          <TextBorder
+            children={"Peer Grading text 2"}
+            imageSrc={"/assets/verified-icon.png"}
+          />
+          <Box w="70px" h="10" />
+          <Image src="/assets/first-page-image.png" w="500px" h="600px" />
         </Flex>
-      </div></>
+      </div>
+    </>
   );
 }
