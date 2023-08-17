b96af17b3945618fd162fca10518bebb962090ab
Pedro <pedroleale@gmail.com>
2023-08-17 11:05:24 -0300

    feat: Home Page placeholder

--- front-end/pages/home.tsx
+++ front-end/pages/home.tsx
@@ -1,15 +1,22 @@
 import { Navbar } from "@/components/Navbar";
+import { TextBorder } from "../components/TextBorder";
+import { Flex, Image, Box } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <div className="flex justify-center">
-        <div className="flex justify-around w-1/2">
-          <span>aqui</span>
-          <span>aqui2</span>
-        </div>
-      </div>
-    </>
+    <><Navbar />
+    <div className=" w-1000 h-screen flex justify-center items-center">
+        <Flex direction="row">
+        <TextBorder children={"Peer Grading text 1"} imageSrc={"/assets/security-icon.png"}/>
+        <Box w='70px' h='10'/>
+        <TextBorder children={"Peer Grading text 2"} imageSrc={"/assets/verified-icon.png"}/>
+        <Box w='70px' h='10'/>
+        <Image
+            src="/assets/first-page-image.png"
+            w="500px"
+            h="600px"
+          />
+        </Flex>
+      </div></>
   );
 }
