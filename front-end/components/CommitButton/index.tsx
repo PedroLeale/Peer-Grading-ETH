65a2ad5d0379fc9085625472a891441d2e06611e
pedro bufulin <pedro.almeida@ufu.br>
2023-12-12 20:32:55 -0300

    commit modal

--- front-end/components/CommitButton/index.tsx
+++ front-end/components/CommitButton/index.tsx
@@ -4,6 +4,8 @@ import { useAccount } from "wagmi";
 import abi from "@/abi/RandomnessSource.json";
 import { useEffect } from "react";
 import { useCommit } from "@/lib/wagmi/useCommit";
+import { useDisclosure } from "@chakra-ui/react";
+import { BaseModal } from "../BaseModal";
 
 interface IVotedButton {
   addedParticipants: AddedParticipant[];
@@ -14,7 +16,8 @@ export const CommitButton = ({ addedParticipants, randSrc }: IVotedButton) => {
   const { address } = useAccount();
   console.log({ randSrc, abi });
 
-  const { write, error } = useCommit({ randSrc, _commit: "asaofsdaifj" });
+  const { /* write */ error } = useCommit({ randSrc, _commit: "asaofsdaifj" });
+  const { onClose, onOpen, isOpen } = useDisclosure();
 
   useEffect(() => {
     if (error) {
@@ -32,12 +35,22 @@ export const CommitButton = ({ addedParticipants, randSrc }: IVotedButton) => {
         <button
           className="bg-[#0096FF] text-white font-bold py-2 px-4 rounded cursor-pointer"
           onClick={() => {
-            write?.();
+            onOpen();
           }}
-          disabled={!write}
+          // disabled={!write}
         >
           commit
         </button>
+
+        <BaseModal title="teste" isOpen={isOpen} onClose={onClose}>
+          {/* TODO: 
+          Colocar um input, uma checkbox para salvar nos cookies,
+          // e o botão final para fazer o commit
+           
+          
+          */}
+          <p> teste</p>
+        </BaseModal>
       </div>
     );
   else return <div></div>;
