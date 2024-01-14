0e25f07ffe06a98f31eeda27c8c291f87cee85f8
pedro bufulin <pedro.almeida@ufu.br>
2024-01-13 21:10:40 -0300

    feat: bump graph version

--- front-end/lib/services/apollo/client.ts
+++ front-end/lib/services/apollo/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api.studio.thegraph.com/query/53131/peer-grading-subgraph/v0.0.14",
+  uri: "https://api.studio.thegraph.com/query/53131/peer-grading-subgraph/v0.0.15",
   cache: new InMemoryCache(),
 });
