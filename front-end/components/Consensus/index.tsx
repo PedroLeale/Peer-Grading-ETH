88b44de6ab6cf7bfc2a9b05d9a32bcbf7654f0f1
pedro bufulin <pedro.almeida@ufu.br>
2024-01-25 16:43:10 -0300

    feat: display votes

--- front-end/components/Consensus/index.tsx
+++ front-end/components/Consensus/index.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@apollo/client";
 import { useEffect } from "react";
 import { useAccount, useContractRead } from "wagmi";
 import abi from "@/abi/PeerGrading.json";
+import { GET_VOTES } from "@/lib/services/apollo/queries/getVotes";
 
 interface IConsensus {
   contract: string;
@@ -12,6 +13,14 @@ export const Consensus = ({ contract }: IConsensus) => {
   const { address } = useAccount();
 
   const { data: consensusData } = useQuery(GET_CONSENSUSES, {
+    variables: {
+      contract: String(contract),
+      skip: 0,
+      first: 10,
+    },
+  });
+
+  const { data: votesData } = useQuery(GET_VOTES, {
     variables: {
       contract: String(contract),
       skip: 0,
@@ -31,6 +40,10 @@ export const Consensus = ({ contract }: IConsensus) => {
     console.log({ data });
   }, [data]);
 
+  useEffect(() => {
+    console.log({ votesData, consensusData });
+  }, [consensusData, votesData]);
+
   return (
     <div>
       {data ? (
@@ -45,16 +58,36 @@ export const Consensus = ({ contract }: IConsensus) => {
       )}
 
       <div className="mt-4 p-4 bg-gray-100 rounded-md">
-        {consensusData?.consensuses.map((consensus: any, index: number) => (
-          <p key={index} className="text-gray-700 text-lg font-bold">
-            Consensus array:{" "}
-            {consensusData?.consensuses.length === 0 ? (
-              <span className="text-red">Empty</span>
-            ) : (
-              consensus.vector.join(" ")
-            )}
-          </p>
-        ))}
+        {consensusData?.consensuses
+          .slice()
+          .sort((a, b) => Number(b.index) - Number(a.index)) // sorts the array by the 'index' property
+          .map((consensus: any, index: number) => (
+            <p key={index} className="text-gray-700 text-lg font-bold">
+              Consensus array:{" "}
+              {consensusData?.consensuses.length === 0 ? (
+                <span className="text-red">Empty</span>
+              ) : (
+                consensus.vector.join(" ")
+              )}
+              <div>
+                <span className="font-bold">
+                  votes on this array:
+                  <br />
+                </span>
+
+                {votesData &&
+                  consensusData &&
+                  votesData?.voteds
+                    .filter((v) => v.consensusCounter === consensus.index)
+                    .map((v, index) => (
+                      <span key={index}>
+                        {v.participant}
+                        <br />
+                      </span>
+                    ))}
+              </div>
+            </p>
+          ))}
       </div>
     </div>
   );
