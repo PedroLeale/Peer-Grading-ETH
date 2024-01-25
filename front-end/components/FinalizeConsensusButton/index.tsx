7a6d6234c8cbfa62090b20409f44f8e6b516bf4b
Pedro <pedroleale@gmail.com>
2024-01-25 20:35:34 -0300

    fix

--- front-end/components/FinalizeConsensusButton/index.tsx
+++ front-end/components/FinalizeConsensusButton/index.tsx
@@ -14,15 +14,12 @@ export const FinalizeConsensusButton = () => {
 
   const { writeAsync } = useFinalizeConsensus({ PeerGradingAddress: contract });
   const onVote = async () => {
-    // Here you can add the logic to handle the vote using the PeerGrading.json ABI
-    console.log("Vote confirmed");
+    console.log("Finalize Consensus confirmed");
     await writeAsync?.();
     onClose();
   };
 
   return (
-    // make a button that opens a modal, with another button inside the modal
-    // that when clicked, calls the onVote function
     <div className="text-left p-4 bg-white rounded-lg w-1/2">
       <button
         className="bg-[#0096FF] text-white font-bold py-2 px-4 rounded cursor-pointer"
